5e35ba9c3d0b3d408ec17c54d7923bd2ed533a61
Justin <40804543+JustinWebDev@users.noreply.github.com>
2018-12-26 16:00:38 -0600

    experiment with toggle shadow on hamburger menu icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
     //console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
+    document.querySelector("button").classList.toggle("hide");
 
 }
