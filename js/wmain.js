0594d62c6fab06e6caa2d815d34afe687c7537cb
Justin <40804543+JustinWebDev@users.noreply.github.com>
2021-01-15 17:47:05 -0600

    week day # corrected, to avoid 'undefined'

--- js/wmain.js
+++ js/wmain.js
@@ -1,18 +1,18 @@
 /*jslint browser:true */
 'use strict';
 
-var weatherConditions = new XMLHttpRequest();
-var weatherForecast = new XMLHttpRequest();
-var wCond; //conditions
-var fObj; //forecast
+let weatherConditions = new XMLHttpRequest();
+let weatherForecast = new XMLHttpRequest();
+let wCond; //conditions
+let fObj; //forecast
 
 document.getElementById('zip').value ="";
 
 function loadWeather() {
-  var zip = document.getElementById('zip').value;
+  let zip = document.getElementById('zip').value;
   if (zip === '') {zip = "35810"}
-  var conditionsPath = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&units=imperial&APPID=8c6405207db6b6fe115ac161f770cd31";
-  var forecastPath = "https://api.openweathermap.org/data/2.5/forecast?zip="+zip+",us&units=imperial&cnt=17&APPID=8c6405207db6b6fe115ac161f770cd31";
+  const conditionsPath = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&units=imperial&APPID=8c6405207db6b6fe115ac161f770cd31";
+  const forecastPath = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zip + ",us&units=imperial&cnt=17&APPID=8c6405207db6b6fe115ac161f770cd31";
 
   // GET THE CONDITIONS
   weatherConditions.open('get', conditionsPath, true);
@@ -30,15 +30,15 @@ weatherConditions.onload = function() {
         wCond = JSON.parse(weatherConditions.responseText);
         console.log(wCond);
         document.getElementById('location').innerHTML = wCond.name;
-        var iconcode = wCond.weather[0].icon;
+        const iconcode = wCond.weather[0].icon;
         console.log("GOT icon = "+ iconcode);
-        var iconpath = "https://openweathermap.org/img/w/"+iconcode+".png";
+        const iconpath = "https://openweathermap.org/img/w/"+iconcode+".png";
         console.log(iconpath);
         document.getElementById('wcicon').src = iconpath;
         // document.getElementById('wcicon').innerHTML = "<img src=\"https://openweathermap.org/img/w/"+wCond.weather[0].icon+".png\">";
         // https://openweathermap.org/weather-conditions  <img src="https://openweathermap.org/img/w/10d.png">
-        var weatherCond = "";
-          for (var i=0; i < wCond.weather.length; i++) {
+        let weatherCond = "";
+          for (let i=0; i < wCond.weather.length; i++) {
             weatherCond += wCond.weather[i].main;
             if (i != wCond.weather.length-1){
               weatherCond += ", ";}
@@ -46,8 +46,8 @@ weatherConditions.onload = function() {
       document.getElementById('weather').innerHTML = weatherCond;
       document.getElementById('temperature').innerHTML = Math.round(wCond.main.temp);
       document.getElementById('windspeed').innerHTML = wCond.wind.speed;
-      var weatherDesc = "";
-        for (var i=0; i < wCond.weather.length; i++) {
+      let weatherDesc = "";
+        for (let i=0; i < wCond.weather.length; i++) {
           weatherDesc += wCond.weather[i].description;
           if (i != wCond.weather.length-1){
             weatherDesc += ", ";}
@@ -63,8 +63,9 @@ if (weatherForecast.status === 200){
 	fObj = JSON.parse(weatherForecast.responseText);
 	console.log(fObj);
   // day 1
-  var d1 = new Date();
-  var weekday1 = new Array();
+  const d1 = new Date();
+  const dayWeek = d1.getDay();
+  const weekday1 = new Array();
   weekday1[0] = "Sunday";
   weekday1[1] = "Monday";
   weekday1[2] = "Tuesday";
@@ -72,37 +73,30 @@ if (weatherForecast.status === 200){
   weekday1[4] = "Thursday";
   weekday1[5] = "Friday";
   weekday1[6] = "Saturday";
-  var day1 = weekday1[d1.getDay()];
+  const day1 = weekday1[dayWeek];
   document.getElementById('r1c1').innerHTML = day1;
   document.getElementById('r1c3').innerHTML = Math.round(fObj.list[0].main.temp)+"&deg;";
   document.getElementById('r1c2').src = "https://openweathermap.org/img/w/"+fObj.list[0].weather[0].icon+".png";
 
   // day 2
-  var d2 = new Date();  // don't need this! just use d1, can genericize to weekday instead of 1,2,3?
-  var weekday2 = new Array();
-  weekday2[0] = "Sunday";
-  weekday2[1] = "Monday";
-  weekday2[2] = "Tuesday";
-  weekday2[3] = "Wednesday";
-  weekday2[4] = "Thursday";
-  weekday2[5] = "Friday";
-  weekday2[6] = "Saturday";
-  var day2 = weekday2[d2.getDay()+1];  // <- here is the problem for undefin ed day!
+  //const d2 = new Date();  // don't need this! just use d1, can genericize to weekday instead of 1,2,3?
+  if (dayWeek === 6) {
+    const day2 = weekday1[0];
+  }
+    // <- here is the problem for undefined day!
   document.getElementById('r2c1').innerHTML = day2;
   document.getElementById('r2c3').innerHTML = Math.round(fObj.list[8].main.temp)+"&deg;";
   document.getElementById('r2c2').src = "https://openweathermap.org/img/w/"+fObj.list[8].weather[0].icon+".png";
 
   // day 3
-  var d3 = new Date();
-  var weekday3 = new Array();
-  weekday3[0] = "Sunday";
-  weekday3[1] = "Monday";
-  weekday3[2] = "Tuesday";
-  weekday3[3] = "Wednesday";
-  weekday3[4] = "Thursday";
-  weekday3[5] = "Friday";
-  weekday3[6] = "Saturday";
-  var day3 = weekday3[d3.getDay()+2]; console.log(day3);
+  //const d3 = new Date();
+  if (dayWeek === 5) {
+    const day3 = weekday1[0];
+  }
+  if (dayWeek === 6) {
+    const day3 = weekday1[1];
+  }
+  console.log(day3);
   document.getElementById('r3c1').innerHTML = day3;
   document.getElementById('r3c3').innerHTML = Math.round(fObj.list[16].main.temp)+"&deg;";
   document.getElementById('r3c2').src = "https://openweathermap.org/img/w/"+fObj.list[16].weather[0].icon+".png";
